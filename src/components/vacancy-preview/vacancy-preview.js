ea2b5dc336d1374d681783212455b35aa5a401c9
Pavel Tataurov <p.tataurov92@gmail.com>
2019-04-25 20:39:11 +0300

    optimization

--- src/components/vacancy-preview/vacancy-preview.js
+++ src/components/vacancy-preview/vacancy-preview.js
@@ -1,45 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import './vacancy-preview.scss'
 
 import { truncate } from 'lodash'
 
-const VacancyPreview = ({ vacancy }) => {
-  const {
-    id,
-    position,
-    companyName,
-    salary,
-    vacancyDesc,
-    address,
-    publishDate
-  } = vacancy
+class VacancyPreview extends Component {
+  shouldComponentUpdate(nextProps) {
+    return nextProps.vacancy.id !== this.props.vacancy.id
+  }
+  render() {
+    const {
+      id,
+      position,
+      companyName,
+      salary,
+      vacancyDesc,
+      address,
+      publishDate
+    } = this.props.vacancy
 
-  return (
-    <Link
-      to={`/vacancy/${id}`}
-      className="vacancy-preview card mb-4"
-      data-id={id}
-    >
-      <div className="card-body">
-        <div>
-          <h6 className="h4 m-0">{position}</h6>
-          <p className="mb-2 text-muted small">{companyName}</p>
-          <p className="h6 m-0">{salary}</p>
+    return (
+      <Link
+        to={`/vacancy/${id}`}
+        className="vacancy-preview card mb-4"
+        data-id={id}
+      >
+        <div className="card-body">
+          <div>
+            <h6 className="h4 m-0">{position}</h6>
+            <p className="mb-2 text-muted small">{companyName}</p>
+            <p className="h6 m-0">{salary}</p>
+          </div>
+          <hr />
+          <p className="m-0">
+            {truncate(vacancyDesc, { length: 250, separator: '...' })}
+          </p>
+          <hr />
+          <div className="d-flex flex-wrap justify-content-between text-muted">
+            {address && <small>{address}</small>}
+            <small className="text-nowrap">{publishDate}</small>
+          </div>
         </div>
-        <hr />
-        <p className="m-0">
-          {truncate(vacancyDesc, { length: 250, separator: '...' })}
-        </p>
-        <hr />
-        <div className="d-flex flex-wrap justify-content-between text-muted">
-          {address && <small>{address}</small>}
-          <small className="text-nowrap">{publishDate}</small>
-        </div>
-      </div>
-    </Link>
-  )
+      </Link>
+    )
+  }
 }
 
 VacancyPreview.propTypes = {
